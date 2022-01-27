6ad225283c67aeb82e0571c733c3b4ab623c0718
Timothy Gaither <timjgaither@gmail.com>
2022-01-27 18:27:15 +0100

    push

---
+++ jsnack3/main.js
@@ -0,0 +1,10 @@
+// 3-Calcola la somma e la media dei primi 10 numeri.
+let media
+let somma=0;
+for(let i=1;i<11; i++){
+    somma+=i;
+    media=somma/i
+    
+}
+alert("La somma è "+somma)
+alert("La media è "+media)
