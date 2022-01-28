8f05d65dcde2563f7c6e095dfb5b9b42bd466396
Timothy Gaither <timjgaither@gmail.com>
2022-01-28 10:33:00 +0100

    push

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -1,6 +1,19 @@
 // 2-Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente.
 
-let cubo;
+/*let cubo;
 let numeroUtente=parseInt(prompt('Inserisci un numero per verificare il suo cubo'));
 
-alert(numeroUtente **= 3);
+console.log(numeroUtente **= 3);
+*/
+
+const numero=parseInt(prompt('inserisci il valore fino a quando ti stampo il cubo'));
+
+if(isNaN(numero) || numero<=0){
+    alert('Inserisci un numero valido');
+}else{
+    for(let i= 1; i<=numero; i++){
+        const cubo=Math.pow(i,3);
+
+        console.log(cubo)
+    }
+}
